fbbb6b5183c0fdfc8d57121ccef5d8a5b7ebd13d
p1nkypasta <98198569+p1nkypasta@users.noreply.github.com>
2022-04-20 19:20:08 -0400

    Update morse_translator.py

--- morse_translator.py
+++ morse_translator.py
@@ -1,64 +1,40 @@
-letters_to_morse = {
+morse = {
     'a' : '.-',
-    'A' : '.-',
     'b' : '-...',
-    'B' : '-...',
     'c' : '-.-.',
-    'C' : '-.-.',
     'd' : '-..',
-    'D' : '-..',
     'e' : '.',
-    'E' : '.',
     'f' : '..-.',
-    'F' : '..-.',
     'g' : '--.',
-    'G' : '--.',
     'h' : '....',
-    'H' : '....',
     'i' : '..',
-    'I' : '..',
     'j' : '.---',
-    'J' : '.---',
     'k' : '-.-',
-    'K' : '-.-',
     'l' : '.-..',
-    'L' : '.-..',
     'm' : '--',
-    'M' : '--',
     'n' : '-.',
-    'N' : '-.',
     'o' : '---',
-    'O' : '---',
     'p' : '.--.',
-    'P' : '.--.',
     'q' : '--.-',
-    'Q' : '--.-',
     'r' : '.-.',
-    'R' : '.-.',
     's' : '...',
-    'S' : '...',
     't' : '-',
-    'T' : '-',
     'u' : '..-',
-    'U' : '..-',
     'v' : '...-',
-    'V' : '...-',
     'w' : '.--',
-    'W' : '.--',
     'x' : '-..-',
-    'X' : '-..-',
     'y' : '-.--',
-    'Y' : '-.--',
     'z' : '--..',
-    'Z' : '--..',
     ' ' : '/'
 }
 
 def morse_code(phrase):
-    print('input something to translate')
     translation = ''
     for letter in phrase:
-        translation += letters_to_morse[letter]
+        if letter.isupper():
+            translation += morse[letter.lower()]
+        else:
+            translation += morse[letter]
     print(translation)
 
-morse_code(input('input phrase to translate --->'))
+morse_code(input('Input phrase to translate ---> '))
